d76f7864267799e289b4fc07537ec9acee080162
sharonpeevor <shazpeevor@hotmail.com>
2022-06-14 11:41:09 +0100

    added a new project

---
+++ src/com/qa/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.qa.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("something else");
+	}
+
+}
